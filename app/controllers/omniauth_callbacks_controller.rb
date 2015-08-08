0b70393255a3dc627f16c122ed19bda0e1ee8fd5
RMCornell <zen.alchemist5280@gmail.com>
2015-08-08 09:47:00 -0600

    devise working.  single user with multiple social media accounts.  or how I just made devise my bitch

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -27,7 +27,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: provider.capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = env['omniauth.auth']
-      redirect_to new_user_registration_url
+      redirect_to root_path
     end
   end
 end
